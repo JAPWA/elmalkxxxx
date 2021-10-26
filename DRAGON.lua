ccad3b2aed05bcc966cf5848b718cb794f18d5a6
TIMOXBODY <93005430+TIMOXBODY@users.noreply.github.com>
2021-10-26 07:06:55 +0200

    Add files via upload

--- DRAGON.lua
+++ DRAGON.lua
@@ -13708,6 +13708,30 @@ end end
 tdcli_function ({ ID = "GetUserProfilePhotos", user_id_ = msg.sender_user_id_, offset_ = 0, limit_ = 1 }, getpro, nil) 
 end,nil)
 end
+if text == "حاجتي" and not database:get(bot_id..'ghiktr7'..msg.chat_id_) then     
+tdcli_function ({ID = "GetUser",user_id_ = msg.sender_user_id_},function(extra,result,success)
+if result.username_ then
+username = result.username_ 
+else
+username = 'e_l_m_o_l_k'
+end
+local msg_id = msg.id_/2097152/0.5  
+local Sasa3 = 'https://t.me/xxxcccvvbbnn/903'
+keyboard = {} 
+keyboard.inline_keyboard = {
+{
+{text = 'اخر صوره لديك', url = "https://t.me/"..result.username_..""},
+},
+}
+local function getpro(extra, result, success) 
+if result.photos_[0] then 
+https.request("https://api.telegram.org/bot"..token..'/sendPhoto?chat_id=' .. msg.chat_id_ .. '&photo='..result.photos_[0].sizes_[1].photo_.persistent_id_..'&photo=' .. URL.escape(textt).."&reply_to_message_id="..msg_id.."&parse_mode=markdown&disable_web_page_preview=true&reply_markup="..JSON.encode(keyboard)) 
+else 
+https.request("https://api.telegram.org/bot"..token..'/sendPhoto?chat_id=' .. msg.chat_id_ .. '&photo=' .. URL.escape(Sasa3).."&reply_to_message_id="..msg_id.."&parse_mode=markdown&disable_web_page_preview=true&reply_markup="..JSON.encode(keyboard)) 
+end end 
+tdcli_function ({ ID = "GetUserProfilePhotos", user_id_ = msg.sender_user_id_, offset_ = 0, limit_ = 1 }, getpro, nil) 
+end,nil)
+end
 if text == 'ايديي' then
 send(msg.chat_id_, msg.id_,' ❤ ايديك  ⇦↯'..msg.sender_user_id_)
 end
