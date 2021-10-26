8649d8820551b3b5a02510f28b6d28872b41dd08
TIMOXBODY <93005430+TIMOXBODY@users.noreply.github.com>
2021-10-26 07:23:45 +0200

    Add files via upload

--- DRAGON.lua
+++ DRAGON.lua
@@ -1645,30 +1645,6 @@ Text = ' ❤ الاحصائيات  \n'..' ❤ عدد الجروبات  ⇦↯{'.
 send(msg.chat_id_, msg.id_,Text) 
 return false
 end
-if text == "حاجتي" and not database:get(bot_id..'ghiktr7'..msg.chat_id_) then     
-tdcli_function ({ID = "GetUser",user_id_ = msg.sender_user_id_},function(extra,result,success)
-if result.username_ then
-username = result.username_ 
-else
-username = 'e_l_m_o_l_k'
-end
-local msg_id = msg.id_/2097152/0.5  
-local Sasa3 = 'https://t.me/xxxcccvvbbnn/903'
-keyboard = {} 
-keyboard.inline_keyboard = {
-{
-{text = 'اخر صوره لديك', url = "https://t.me/"..result.username_..""},
-},
-}
-local function getpro(extra, result, success) 
-if result.photos_[0] then 
-https.request("https://api.telegram.org/bot"..token..'/sendPhoto?chat_id=' .. msg.chat_id_ .. '&photo='..result.photos_[0].sizes_[1].photo_.persistent_id_..'&photo=' .. URL.escape(textt).."&reply_to_message_id="..msg_id.."&parse_mode=markdown&disable_web_page_preview=true&reply_markup="..JSON.encode(keyboard)) 
-else 
-https.request("https://api.telegram.org/bot"..token..'/sendPhoto?chat_id=' .. msg.chat_id_ .. '&photo=' .. URL.escape(Sasa3).."&reply_to_message_id="..msg_id.."&parse_mode=markdown&disable_web_page_preview=true&reply_markup="..JSON.encode(keyboard)) 
-end end 
-tdcli_function ({ ID = "GetUserProfilePhotos", user_id_ = msg.sender_user_id_, offset_ = 0, limit_ = 1 }, getpro, nil) 
-end,nil)
-end
 if text == 'المشتركين' and SudoBot(msg) then 
 local Groups = database:scard(bot_id..'Chek:Groups')  
 local Users = database:scard(bot_id..'User_Bot')  
@@ -13403,6 +13379,38 @@ Text = ' ❤ الاحصائيات  \n'..' ❤ عدد الجروبات  ⇦↯{'.
 send(msg.chat_id_, msg.id_,Text) 
 end
 return false
+end
+if text == "حاجتي" then
+local my_ph = bot_data:get(ban_id.."my_photo:status"..msg.chat_id_)
+if not my_ph 
+return false  
+end
+tdcli_function ({ID = "GetUser",user_id_ = msg.sender_user_id_},function(extra,result,success)
+if result.username_ then
+username = result.username_ 
+else
+username = 'sasa_boody'
+end
+local msg_id = msg.id_/2097152/0.5  
+local texzxtt = ' اخر صوره لديك'
+local Boody = 'https://t.me/xxxcccvvbbnn/903'
+keyboard = {} 
+keyboard.inline_keyboard = {
+{
+{text = texzxtt, url="http://t.me/"..username},
+},
+{
+{text = 'اضف البوت الي مجموعتك' ,url="t.me/"..dofile("./Info.lua").botUserName.."?startgroup=start"},
+},
+}
+local function getpro(extra, result, success) 
+if result.photos_[0] then 
+https.request("https://api.telegram.org/bot"..token..'/sendPhoto?chat_id=' .. msg.chat_id_ .. '&photo='..result.photos_[0].sizes_[1].photo_.persistent_id_..'&photo=' .. URL.escape(textt).."&reply_to_message_id="..msg_id.."&parse_mode=markdown&disable_web_page_preview=true&reply_markup="..JSON.encode(keyboard)) 
+else 
+https.request("https://api.telegram.org/bot"..token..'/sendPhoto?chat_id=' .. msg.chat_id_ .. '&photo=' .. URL.escape(Boody).."&reply_to_message_id="..msg_id.."&parse_mode=markdown&disable_web_page_preview=true&reply_markup="..JSON.encode(keyboard)) 
+end end 
+tdcli_function ({ ID = "GetUserProfilePhotos", user_id_ = msg.sender_user_id_, offset_ = 0, limit_ = 1 }, getpro, nil) 
+end,nil)
 end
 if text == 'الجروبات' then
 if Sudo(msg) then 
