b0a50c173d2fd917a928b166fe6ba8bbed3d88e6
TIMOXBODY <93005430+TIMOXBODY@users.noreply.github.com>
2021-10-30 05:53:13 +0200

    Add files via upload

--- DRAGON.lua
+++ DRAGON.lua
@@ -146,14 +146,17 @@ end
 _redis = load_redis()  
 --------------------------------------------------------------------------------------------------------------
 print([[
-  ______ _      __  __ _      ____  _  __
- |  ____| |    |  \/  | |    / __ \| |/ /
- | |__  | |    | \  / | |   | |  | | ' / 
- |  __| | |    | |\/| | |   | |  | |  <  
- | |____| |____| |  | | |___| |__| | . \ 
- |______|______|_|  |_|______\____/|_|\_\
-                                         
-                                         
+      ___           ___       ___           ___       ___           ___     
+     /\  \         /\__\     /\__\         /\__\     /\  \         /\__\    
+    /::\  \       /:/  /    /::|  |       /:/  /    /::\  \       /:/  /    
+   /:/\:\  \     /:/  /    /:|:|  |      /:/  /    /:/\:\  \     /:/__/     
+  /::\~\:\  \   /:/  /    /:/|:|__|__   /:/  /    /:/  \:\  \   /::\__\____ 
+ /:/\:\ \:\__\ /:/__/    /:/ |::::\__\ /:/__/    /:/__/ \:\__\ /:/\:::::\__\
+ \:\~\:\ \/__/ \:\  \    \/__/~~/:/  / \:\  \    \:\  \ /:/  / \/_|:|~~|~   
+  \:\ \:\__\    \:\  \         /:/  /   \:\  \    \:\  /:/  /     |:|  |    
+   \:\ \/__/     \:\  \       /:/  /     \:\  \    \:\/:/  /      |:|  |    
+    \:\__\        \:\__\     /:/  /       \:\__\    \::/  /       |:|  |    
+     \/__/         \/__/     \/__/         \/__/     \/__/         \|__|    
 
                                     
 > CH ♔ »@e_l_m_o_l_k«
@@ -16824,7 +16827,7 @@ local Teext =[[
  ♔  مسح الكلمات الممنوعه
  ♔ المميزين عام
  ♔  ❲ + ❳ جميع ماسبق
- ??━┅┄⟞⟦𝚂𝙾𝚄𝚁𝙲𝙴 𝙴??𝙼𝙻𝙾𝙺⟧⟝┄┉━??
+ ??━┅┄⟞⟦𝚂𝙾𝚄𝚁𝙲𝙴 𝙴𝙻𝙼𝙻𝙾𝙺⟧⟝┄┉━??
  ♔ ❲ المالك❳ ♔
  ♽^━━━━❆𝗘𝗟𝗠𝗟𝗢𝗞❆━━━━^♽ 
  ♔ اضف صوره ♢ وصف (للجروب)
@@ -17668,8 +17671,8 @@ local Teext =[[
 ]]
 keyboard = {} 
 keyboard.inline_keyboard = {
-{{text = 'Developers', callback_data="/units"},{text = 'ᴍʏ ᴄʜᴀɴɴᴇʟ', callback_data="/theytry"}},   
-{{text = 'مــطــور الــبــوت', url="http://t.me/"..sudos.UserName}},
+{{text = '♔ 𝙼𝚈 𝙳𝙴𝚅', callback_data="/units"},{text = '♔ 𝙼𝚈 𝙲𝙷𝙰𝙽𝙽𝙴𝙻', callback_data="/theytry"}},   
+{{text = 'الــمـــطــور ♔', url="http://t.me/"..sudos.UserName}},
 }
 return https.request("https://api.telegram.org/bot"..token..'/editMessagecaption?chat_id='..Chat_id..'&caption='..URL.escape(Teext)..'&message_id='..msg_idd..'&disable_web_page_preview=true&reply_markup='..JSON.encode(keyboard))  
 end
@@ -17679,8 +17682,8 @@ local Teext =[[
 ]]
 keyboard = {} 
 keyboard.inline_keyboard = {
-{{text = '《𝚂𝙾𝚄𝚁𝙲𝙴 𝙴𝙻𝙼𝙻𝙾𝙺》 ', url="t.me/e_l_m_o_l_k"}},
-{{text = 'BACK', callback_data="/HHH"}},
+{{text = '♔ 𝚂𝙾𝚄𝚁𝙲𝙴 𝙴𝙻𝙼𝙻𝙾𝙺 ', url="t.me/e_l_m_o_l_k"}},
+{{text = '♔ BACK', callback_data="/HHH"}},
 }
 return https.request("https://api.telegram.org/bot"..token..'/editMessagecaption?chat_id='..Chat_id..'&caption='..URL.escape(Teext)..'&message_id='..msg_idd..'&disable_web_page_preview=true&reply_markup='..JSON.encode(keyboard))  
 end
@@ -17690,9 +17693,9 @@ local Teext =[[
 ]]
 keyboard = {} 
 keyboard.inline_keyboard = {
-{{text = '𝚃𝙸𝙼𝙾',url="t.me/ttimo_2lby"},{text = '𝙹𝙰𝙱𝚆𝙰',url="t.me/J_A_B_W_A"}},
-{{text = '𝙾𝙼𝙰𝚁',url="t.me/teemElmelok"},{text = '𝙰𝙱𝙾 𝙴𝙻𝙼𝙰𝙶𝙳',url="t.me/ABO_ELMAGD2"}},
-{{text = 'BACK', callback_data="/HHH"}},
+{{text = '♔ 𝚃𝙸𝙼𝙾',url="t.me/ttimo_2lby"},{text = '♔ 𝙹𝙰𝙱𝚆𝙰',url="t.me/J_A_B_W_A"}},
+{{text = '♔ 𝙾𝙼𝙰𝚁',url="t.me/teemElmelok"},{text = '♔ 𝙰𝙱𝙾 𝙴𝙻𝙼𝙰𝙶𝙳',url="t.me/ABO_ELMAGD2"}},
+{{text = '♔ BACK', callback_data="/HHH"}},
 }
 return https.request("https://api.telegram.org/bot"..token..'/editMessagecaption?chat_id='..Chat_id..'&caption='..URL.escape(Teext)..'&message_id='..msg_idd..'&disable_web_page_preview=true&reply_markup='..JSON.encode(keyboard))  
 end
